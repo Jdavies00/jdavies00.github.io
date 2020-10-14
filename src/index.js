66036e48b5d1dfd3e2e3bb7feaae50f82a5d3736
James Davies <j.davies2099@gmail.com>
2020-10-14 16:26:28 -0400

    json data

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import 'bootstrap/dist/css/bootstrap.css';
+
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
